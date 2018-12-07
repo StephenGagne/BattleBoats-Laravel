9538d57a0f2521e5d8dfb31286d90d40d9c41b75
pepi0039 <pepi0039@algonquinlive.com>
2018-12-07 08:37:20 -0500

    Timer

--- public/js/game.js
+++ public/js/game.js
@@ -180,3 +180,11 @@ function buildGrid() {
     }
     gameRunning = true
 }
+
+const timer = 30;
+
+const interval = setInterval(function() {
+    timer--;
+    $('.timer').text(timer);
+    if (timer === 0) clearInterval(interval);
+}, 1000);
